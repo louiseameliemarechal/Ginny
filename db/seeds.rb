be3cb0accaf77e2c9e3fc6f409b744047b7c0a0e
LouiseAmelie Marechal <louiseamelie2@gmail.com>
2021-05-24 17:36:10 +0200

    seed created, still need to remove duplicates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,25 @@
 require 'json'
 require 'open-uri'
 
-50.times do
-  url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=medecins&q=&facet=civilite&facet=column_12&facet=column_13&facet=column_14&facet=column_16&facet=libelle_profession&facet=type_dacte_realise&facet=commune&facet=nom_epci&facet=nom_dep&facet=nom_reg&facet=insee_reg&facet=insee_dep&facet=libelle_regroupement&facet=libelle&facet=libelle_acte_clinique'
-  serialized_doctors = URI.open(url).open
+Doctor.destroy_all
+
+  url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=medecins&q=&rows=50&facet=civilite&facet=column_12&facet=column_13&facet=column_14&facet=column_16&facet=libelle_profession&facet=type_dacte_realise&facet=commune&facet=nom_epci&facet=nom_dep&facet=nom_reg&facet=insee_reg&facet=insee_dep&facet=libelle_regroupement&facet=libelle&facet=libelle_acte_clinique&refine.libelle_profession=Gyn%C3%A9cologue+obst%C3%A9tricien'
+  serialized_doctors = URI.open(url).read
   doctors = JSON.parse(serialized_doctors)
-  new_doctor = Doctor.create(
-    first_name: records[2]['nom'],
-    last_name: records[2]['nom'],
-    address: records[2]['adresse'],
-    specialty: records[2]['nom_acte'],
-    profession: records[2]['libelle_profession'],
-    convention: records[2]['column_14'],
-    sexe: records[2]['civilite'],
-    average_price: records[2]['tarif_2'],
+
+doctors['records'].each do |record|
+  new_doctor = Doctor.create!(
+    first_name: record['fields']['nom'].split.first,
+    last_name: record['fields']['nom'].split.last,
+    address: record['fields']['adresse'],
+    specialty: record['fields']['nom_acte'],
+    profession: record['fields']['libelle_profession'],
+    convention: record['fields']['column_14'],
+    gender: record['fields']['civilite'],
+    average_number: record['fields']['tarif_2']
     )
-  puts new_doctor.first_name + ' created' if new_book.save
+    puts "#{new_doctor.first_name} #{new_doctor.last_name} created" if new_doctor.save
 end
+
+
+
