d0ac79f4fe282e8e76f11fb7549773d2ac3af183
LouiseAmelie Marechal <louiseamelie2@gmail.com>
2021-05-24 16:00:58 +0200

    all has many added to related models

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,2 +1,3 @@
 class Badge < ApplicationRecord
+  has_many :badge_recos
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,4 @@
 class Doctor < ApplicationRecord
+  has_many :recommendations
   validates :first_name, :last_name, :address, :profession, :gender, presence: true
 end
