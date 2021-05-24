1aa6ab3c139f9f7d435598e6c2c7a89f1433bb9e
LouiseAmelie Marechal <louiseamelie2@gmail.com>
2021-05-24 17:00:26 +0200

    creating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+50.times do
+  url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=medecins&q=&facet=civilite&facet=column_12&facet=column_13&facet=column_14&facet=column_16&facet=libelle_profession&facet=type_dacte_realise&facet=commune&facet=nom_epci&facet=nom_dep&facet=nom_reg&facet=insee_reg&facet=insee_dep&facet=libelle_regroupement&facet=libelle&facet=libelle_acte_clinique'
+  serialized_doctors = URI.open(url).open
+  doctors = JSON.parse(serialized_doctors)
+  new_doctor = Doctor.create(
+    first_name: records[2]['nom'],
+    last_name: records[2]['nom'],
+    address: records[2]['adresse'],
+    specialty: records[2]['nom_acte'],
+    profession: records[2]['libelle_profession'],
+    convention: records[2]['column_14'],
+    sexe: records[2]['civilite'],
+    average_price: records[2]['tarif_2'],
+    )
+  puts new_doctor.first_name + ' created' if new_book.save
+end
