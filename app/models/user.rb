17146b706f6f01e41ca130ba4c7eda9187f78796
LouiseAmelie Marechal <louiseamelie2@gmail.com>
2021-05-24 16:14:29 +0200

    done with the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :recommendations
-  has_many :favorties
+  has_many :favorites
+  has_many :friendships
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
