00b4e81cbcd7de0d4156d227e6173eb7e4e26f50
LouiseAmelie Marechal <louiseamelie2@gmail.com>
2021-05-25 12:35:14 +0200

    changes done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :friendships
   validates :username, presence: true, uniqueness: true
-  validates :phone_number, presence: true, uniqueness: true, format: { with: /\d[0-9]\)*\z/, message: "format invalide" }
+  validates :phone_number, presence: true, uniqueness: true, format: { with: /0[6-7]\d{8}/, message: "format invalide" }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
