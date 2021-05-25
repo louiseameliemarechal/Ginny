8f349197ab6873c1e12f1c3d903b2433c1bcaf8d
melanie grisey <melanie.grisey@hotmail.fr>
2021-05-25 13:23:37 +0200

    added geocoder

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,6 +1,12 @@
 class DoctorsController < ApplicationController
   def index
-
     @doctors = Doctor.all
+
+    @markers = @doctors.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+        info_window: render_to_string(partial: "info_window", locals: { flat: flat })
+      }
   end
 end
