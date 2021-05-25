1d45963b723ea3e9148ca99e50619e9ccf861953
LouiseAmelie Marechal <louiseamelie2@gmail.com>
2021-05-25 15:23:50 +0200

    stimulus added to the continue button to change the text and the progress bar

---
+++ app/javascript/controllers/hello_controller.js
@@ -0,0 +1,20 @@
+// Visit The Stimulus Handbook for more details
+// https://stimulusjs.org/handbook/introduction
+//
+// This example controller works with specially annotated HTML like:
+//
+// <div data-controller="hello">
+//   <h1 data-target="hello.output"></h1>
+// </div>
+
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "step1", "step2", "progress"]
+
+  change() {
+    this.step1Target.classList.add('d-none');
+    this.step2Target.classList.remove('d-none');
+    this.progressTarget.setAttribute("style", 'width: 60%;')
+  }
+}
