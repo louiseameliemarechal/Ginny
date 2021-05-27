7d683cd40b218239cf2d6e2b0afdb833a84076f5
LouiseAmelie Marechal <louiseamelie2@gmail.com>
2021-05-27 16:26:15 +0200

    started

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   end
   resources :favorites, only: [:destroy]
 
+  resources :profiles, only: [:show]
+
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
 end
