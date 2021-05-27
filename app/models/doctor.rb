a1d4a4805e7e7a37ee68f6ae7d7a25c0f2396a80
melanie grisey <melanie.grisey@hotmail.fr>
2021-05-27 15:59:13 +0200

    add search by address and by name_or_specialty

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -8,6 +8,17 @@ class Doctor < ApplicationRecord
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
 
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_specialty, 
+    against: [ 
+      [:last_name, "A"],
+      :first_name,
+      :specialty
+    ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
+
   def self.professions
     pluck(:profession).uniq
   end
