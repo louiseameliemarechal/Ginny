801fe549389b0e35cd7322b10a7a9f979bb1eb1d
LouiseAmelie Marechal <louiseamelie2@gmail.com>
2021-05-28 13:14:55 +0200

    we can now search for a friend by username

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,13 @@
 class ProfilesController < ApplicationController
   def index
     @profiles = User.all
+    if params.dig(:search)
+      @profiles = @profiles.where(username: params.dig(:search, :username))
+    end
   end
 
   def show
     @profile = User.find(params[:id])
     @friendship = current_user.friendships.find_by(friend_id: params[:id])
-  end
-
-  def search
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/search', to: 'pages#search'
-  get '/search', to: 'profiles#search'
   get '/filtre', to: 'doctors#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :doctors, only: [:index, :show] do
