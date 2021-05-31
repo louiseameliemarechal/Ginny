2d5281bdde282fb2b54fafe2b896eff4aba520d4
melanie grisey <melanie.grisey@hotmail.fr>
2021-05-31 12:02:44 +0200

    updated the modal for recommending with JS and CSS

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,13 +1,16 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["checkbox", "errorMessage", "nextButton"]
+  static targets = ["checkbox", "nextButton", "label", "searchForm", "footer", "message"]
 
   change() {
     if (this.checkboxTarget.checked!=true) {
-      this.errorMessageTarget.classList.remove('d-none')
+      this.labelTarget.classList.add("red-color");
     } else {
-      this.nextButtonTarget.click()
+      /* this.nextButtonTarget.click(); */
+      this.searchFormTarget.classList.remove("d-none");
+      this.footerTarget.remove();
+      this.messageTarget.classList.add("d-none");
     }
   }
 }
