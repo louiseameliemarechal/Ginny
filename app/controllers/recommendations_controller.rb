65a58f7818dd99c905d224a066d90664d6e2ae91
melanie grisey <melanie.grisey@hotmail.fr>
2021-06-01 18:24:28 +0200

    integrated css from filter for recommendations/new

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -8,7 +8,11 @@ class RecommendationsController < ApplicationController
   def create
     @user = current_user
     Recommendation.find_by(user: @user, doctor: @doctor).destroy if Recommendation.find_by(user: @user, doctor: @doctor)
-    @recommendation = Recommendation.new(recommendation_params)
+    new_attributes = {
+      badge_ids: recommendation_params[:badge_ids],
+      tags: recommendation_params[:tags].reject(&:empty?).map { |tag_name| Tag.find_by(name: tag_name) }
+    }
+    @recommendation = Recommendation.new(new_attributes)
     @recommendation.user = @user
     @recommendation.doctor = @doctor
     @recommendation.save!
@@ -28,6 +32,6 @@ class RecommendationsController < ApplicationController
   end
 
   def recommendation_params
-    params.require(:recommendation).permit(badge_ids: [], tag_ids: [])
+    params.require(:recommendation).permit(badge_ids: [], tags: [])
   end
 end
