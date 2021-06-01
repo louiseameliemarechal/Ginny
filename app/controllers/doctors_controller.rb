f5a8ccc1ea32a310b331599fe85dbc958ab40903
thibaultfltz <thibault.feltz@gmail.com>
2021-06-01 10:28:56 +0200

    Heroki pic test

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,7 +2,7 @@ class DoctorsController < ApplicationController
   # before_action :set_doctor, only: :edit
 
   def index
-    @doctors = Doctor.near('Paris', 1000)
+    @doctors = Doctor.near('Paris', 10000)
 
     if params.dig(:search, :address).present?
       @doctors = @doctors.near(params.dig(:search, :address), 10)
